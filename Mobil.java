69504e6681829e2727536879486fd70294bd6fc2
redveil07 <127522639+redveil07@users.noreply.github.com>
2026-03-08 21:51:55 +0700

    UPDATE VERSION

--- Mobil.java
+++ Mobil.java
@@ -1,31 +1,48 @@
 public class Mobil {
-private String nopol;
-private String warna;
-private String merk;
-private int kecepatan;
-private int JarakTempuh;
 
-public void setNopol(String n) {
-nopol = n;
-}
-public void setWarna(String s) {
-warna = s;
-}
-public void setMerk(String m) {
-merk = m;
-}
-public void setKecepatan(int k) {
-kecepatan = k;
-}
-public void setJarakTempuh(int j) {
-JarakTempuh = j;
-}
-public void display() {
-System.out.println("Mobil bermerk " + merk);
-System.out.println("bernomor polisi " + nopol);
-System.out.println("serta memililki warna " + warna);
-System.out.println("bergerak dengan kecepatan " + kecepatan + " kpj");
-System.out.println("jarak yang ditempuh " + JarakTempuh + "km");
+    private String nopol;
+    private String warna;
+    private String merk;
+    private int kecepatan;
+    private int JarakTempuh;
 
-}
+    public void setNopol(String n) {
+        nopol = n;
+    }
+
+    public void setWarna(String s) {
+        warna = s;
+    }
+
+    public void setMerk(String m) {
+        merk = m;
+    }
+
+    public void setKecepatan(int k) {
+        kecepatan = k;
+    }
+
+    public void setJarakTempuh(int j) {
+        JarakTempuh = j;
+    }
+
+    public void HitungJarakTempuh(int waktu) {
+        JarakTempuh = kecepatan * waktu;
+    }
+
+    public void kecepatanpersecond() {
+    double KecepatanPerSecond = kecepatan*1000/3600;        
+    System.out.println("Mobil bergerak dalam kecepatan " + KecepatanPerSecond + " mps");
+
+    }
+
+
+
+    public void display() {
+        System.out.println("Mobil bermerk " + merk);
+        System.out.println("bernomor polisi " + nopol);
+        System.out.println("serta memiliki warna " + warna);
+        System.out.println("bergerak dengan kecepatan " + kecepatan + " kpj");
+        System.out.println("jarak yang ditempuh " + JarakTempuh + " km");
+    }
 }
